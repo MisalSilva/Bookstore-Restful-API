bcebcc1888e2b31633fc775544a3e80d9b6ccd86
Misal Silva <misal.20230183@iit.ac.lk>
2025-04-30 14:40:21 +0530

    Delete Bookstore/src/main directory

--- Bookstore/src/main/java/com/api/bookstore/exception/AuthorNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.api.bookstore.exception;
-
-public class AuthorNotFoundException extends RuntimeException {
-    public AuthorNotFoundException(String message) {
-        super(message);
-    }
-}
--- Bookstore/src/main/java/com/api/bookstore/exception/InvalidInputException.java
+++
@@ -1,7 +0,0 @@
-package com.api.bookstore.exception;
-
-public class InvalidInputException extends RuntimeException {
-    public InvalidInputException(String message) {
-        super(message);
-    }
-}
